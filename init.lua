21a604f059b29421337471b55735a44bba0aa269
Jacob Wiseberg <jake@shift1w.com>
2025-12-21 21:40:22 +0100

    true colors to work properly with tmux

--- init.lua
+++ init.lua
@@ -7,6 +7,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Set to true so that colors don't colide when running in tmux
+vim.opt.termguicolors = true
+
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
